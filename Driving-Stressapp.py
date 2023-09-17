8e263fd548c5a0273b504a789149d1dbd43257fa
edenede2 <131803027+edenede2@users.noreply.github.com>
2023-09-17 11:12:29 +0300

    Update Driving-Stressapp.py

--- Driving-Stressapp.py
+++ Driving-Stressapp.py
@@ -304,7 +304,7 @@ def plot_event_analysis_updated(df, selected_event, parameter, offset, pre_event
     # Find the index of the selected event
     event_index = df[df['Event'] == selected_event].index[0]
 
-    # Extract a window of 200 rows centered around the event (100 rows before and after)
+    # Extract a window of rows around the event based on the pre-event and post-event range
     window_start = max(0, event_index - pre_event_range)
     window_end = min(df.shape[0], event_index + post_event_range)
     df_window = df.iloc[window_start:window_end]
@@ -313,17 +313,30 @@ def plot_event_analysis_updated(df, selected_event, parameter, offset, pre_event
     plt.figure(figsize=(12, 6))
     
     # Plot the actual values of the parameter
-    plt.plot(df_window.index, df_window[parameter], label=parameter, color='blue')
+    if parameter == "BrakAcce":
+        # For BrakAcce, check for changes > 0.14 and highlight them
+        changes = df_window[parameter].diff().abs() > 0.14
+        colors = ['red' if change else 'blue' for change in changes]
+        plt.scatter(df_window['Distm'], df_window[parameter], c=colors, label=parameter)
+    else:
+        plt.plot(df_window['Distm'], df_window[parameter], label=parameter, color='blue')
     
-    plt.axvline(x=event_index, color='r', linestyle='--', label='Event')
+    plt.axvline(x=df_window.loc[event_index, 'Distm'], color='r', linestyle='--', label='Event')
     
     # Overlay a scatter plot for the selected offset
     offset_index = event_index + offset
     value_at_offset = df_window.loc[offset_index, parameter]
-    plt.scatter([offset_index], [df_window.loc[offset_index, parameter]], color='green', s=100, zorder=5, label='Selected Offset')
+    plt.scatter([df_window.loc[offset_index, 'Distm']], [df_window.loc[offset_index, parameter]], color='green', s=100, zorder=5, label='Selected Offset')
     
+    # Traffic light color background
+    tl_colors = {0: None, 1: 'green', 2: 'orange', 3: 'red'}
+    for idx, row in df_window.iterrows():
+        color = tl_colors.get(row['TL'])
+        if color:
+            plt.axvspan(row['Distm'], row['Distm'] + df_window['Distm'].diff().median(), facecolor=color, alpha=0.2)
+
     plt.title(f'Change in {parameter} around Event {selected_event}')
-    plt.xlabel('Row Index')
+    plt.xlabel('Distm (Distance)')
     plt.ylabel(parameter)
     plt.legend()
     plt.grid(True)
